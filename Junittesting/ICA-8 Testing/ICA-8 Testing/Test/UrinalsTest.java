f1927225b3cacbed2554391e1bda3ff0fcf46c25
MsBodavula <112110432+MsBodavula@users.noreply.github.com>
2022-10-27 19:26:31 -0700

    Counting the available urinals
    Added the function to count available urinals

---
+++ Junittesting/ICA-8 Testing/ICA-8 Testing/Test/UrinalsTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class UrinalsTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void goodStringTest() {
+        String input = "veda";
+        assertEquals(false, Urinals.goodString(input));
+    }
+
+    @org.junit.jupiter.api.Test
+    void countUrinalsTest() {
+        assertEquals( 1 , Urinals.countUrinals("10001"));
+    }
+}
